abec8b4850becfa752da8b9b5639d44ed5e204bc
RuRuuuu <ayokooghenerukevwe2222@gmail.com>
2022-09-08 16:24:48 -0700

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+
 /**
- * main - main block
- * Return: 1
+ * main - print quote
+ *
+ * Description: prints Dora's quote
+ *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	int s = strlen("and that piece of art useful\" - Dora Korpar, 2015-10-19\n");
-
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
