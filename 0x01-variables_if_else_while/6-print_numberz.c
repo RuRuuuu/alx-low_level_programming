2e6d915a67ad0fd02b20400d5f779dd1452a96fa
RuRuuuu <ayokooghenerukevwe2222@gmail.com>
2022-09-09 15:20:59 -0700

    my commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 20;
+
+	while (i < 30)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
