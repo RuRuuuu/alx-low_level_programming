7eff65fd07f434f3f53ffa64f8d2c6186647eb6d
RuRuuuu <ayokooghenerukevwe2222@gmail.com>
2022-09-14 11:33:23 -0700

    my commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,29 +11,23 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-	for (b = 0; b <= 9; b++)
-	{
-	c = a * b;
-	if ((c / 10) == 0)
-	{
-	if (b == 0)
-	{
-		_putchar (' ');
-		_putchar ((c % 10) + '0');
-	}
-	if (b < 9)
-	{
-		_putchar(',');
-		_putchar (' ');
-	}
-	}
-	else
-	{
-		_putchar ((c / 10) + '0');
-		_putchar (' ');
-	}
-	}
-	}
-	_putchar ('\n');
+		_putchar('0');
+
+		for (b = 1; b <= 9; b++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			c = a * b;
+
+			if (c <= 9)
+				_putchar(' ');
+			else
+				_putchar((c / 10) + '0');
+
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
 	}
 }
+
